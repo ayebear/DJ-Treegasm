bafa4d4a9defe65f47262d0851ac0e71e5503bae
Eric Hebert <ayebear@gmail.com>
2016-03-05 16:29:43 -0500

    Added fractal to standalone page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
+window.addEvent('domready',function() {
+	var ctx = $("tree_canvas");
+	ctx.width  = window.innerWidth - 8;
+	ctx.height = window.innerHeight - 8;
+	var instance = new Tree("tree_canvas");
+})
+
 Leap.loop({background: true}, mainLoop).connect();
 
 function mainLoop(frame) {
-	console.log(frame);
+	// console.log(frame);
 }
